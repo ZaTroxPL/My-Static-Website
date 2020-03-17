2b71300042668eff87bad159d92a83e5c3a4a7f8
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-03-17 11:15:17 +0000

    Created basic structure and color scheme for the homepage, also created error page file with no content

---
+++ Scripts/my-website.homepage.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function() {
+    var viewportHeight = $("#background").height();
+    var navbarHeight = $("#navbar").height();
+
+    var contentHeight = viewportHeight - navbarHeight;
+
+    $("#content").height(contentHeight);
+});*/

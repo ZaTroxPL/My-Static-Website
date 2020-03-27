7034f528c7f9b62040e355d78e667db196c06744
Jakub Panczyszyn <zatroxkp@gmail.com>
2020-03-27 09:59:27 +0000

    commiting changes from 10 days ago, probably just bug fixes and file cleanup

--- Scripts/my-website.homepage.js
+++
@@ -1,8 +0,0 @@
-/*$(document).ready(function() {
-    var viewportHeight = $("#background").height();
-    var navbarHeight = $("#navbar").height();
-
-    var contentHeight = viewportHeight - navbarHeight;
-
-    $("#content").height(contentHeight);
-});*/
